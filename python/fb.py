5507d9d295e368833ff9469dabea32ce38f1df35
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-12-14 19:33:03 +0530

    twitter login

--- python/fb.py
+++ python/fb.py
@@ -4,7 +4,7 @@ from getpass import getpass
 usr = input('Enter username / email ID: ')
 pwd = getpass('Enter password: ')
 
-driver = webdriver.Chrome(r"‪C:\Users\user\Downloads\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome()
 driver.get('https://www.facebook.com/')
 
 username_box = driver.find_element_by_id('email')
@@ -13,5 +13,5 @@ username_box.send_keys(usr)
 password_box = driver.find_element_by_id('pass')
 password_box.send_keys(pwd)
 
-login_btn = driver.find_element_by_id('u_0_2')
+login_btn = driver.find_element_by_id('loginbutton')
 login_btn.submit()
