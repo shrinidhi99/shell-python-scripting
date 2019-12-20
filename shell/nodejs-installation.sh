7a62668d6f1496ad60514e5ad75b9f901f43cdf5
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-12-20 14:27:24 +0530

    node-js installation script

---
+++ shell/nodejs-installation.sh
@@ -0,0 +1,10 @@
+sudo apt install curl
+sudo apt-get install software-properties-common
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+# to check if it is properly installed
+node -v
+npm -v
+node
+# > console.log("Hello Node");
+# > .exit
