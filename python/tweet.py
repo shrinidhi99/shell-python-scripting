53da9a1d27b79edb0bf7027c7f755739232573b6
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-12-22 19:59:00 +0530

    tweeting a message

---
+++ python/tweet.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from getpass import getpass
+from time import sleep
+
+usr = input('Enter username or email: ')
+pwd = input('Enter password: ')
+msg = input('Enter your tweet: ')
+
+driver = webdriver.Chrome()
+driver.get('https://twitter.com/login')
+
+usr_box = driver.find_element_by_class_name('js-username-field')
+usr_box.send_keys(usr)
+sleep(3)
+
+pwd_box = driver.find_element_by_class_name('js-password-field')
+pwd_box.send_keys(pwd)
+sleep(3)
+
+login_button = driver.find_element_by_css_selector('button.submit.EdgeButton.EdgeButton--primary.EdgeButtom--medium')
+login_button.submit()
+sleep(3)
+
+text_box = driver.find_element_by_id('tweet-box-home-timeline')
+text_box.send_keys(msg)
+sleep(3)
+
+tweet_button = driver.find_elements_by_css_selector('button.tweet.action.EdgeButton--primary.js-tweet-btn')
+tweet_button.click()
